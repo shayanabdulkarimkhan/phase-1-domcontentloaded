893f83edec6fa9a1834b1f8e8a0ff06f857686d9
shayan <sak704@nyu.edu>
2022-03-06 16:18:31 -0800

    commit tree 726d1aef8b45acc13c51a52a90140795590dbbd9

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function () {
+    let para = document.body.querySelector("#text");
+    para.textContent = "This is really cool!"
+});
